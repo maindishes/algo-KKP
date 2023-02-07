f2f44dad365745987a89d07b6a6c86e18b930a46
maindishes <manynolan1@gmail.com>
2023-02-07 13:59:23 +0900

    원형 큐 ? solving...

---
+++ cpp/class2/11866.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n,k;
+    std::cin >> n >> k;
+    std::vector<int> my_vec;
+    int f =0;
+    int r = 0;
+    for(int i=0; i<n; ++i) {
+        my_vec.push_back(i+1);
+    }
+    int r = my_vec.size();
+    for (int i =0; i<)
+
+}
+
