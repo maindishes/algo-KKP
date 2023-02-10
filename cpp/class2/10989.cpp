fcb960cb843538d84763b7f0d8bc2a3407e63e4d
maindishes <manynolan1@gmail.com>
2023-02-10 16:24:34 +0900

    메모리 초과 해결 : 딱주어진 대로 만 한번 쓰게 해결
    - std::ios::sync_with_stdio(false);
    - std::cin.tie(0);

--- cpp/class2/10989.cpp
+++ cpp/class2/10989.cpp
@@ -12,27 +12,35 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
+#include <functional>
+#include <cstdio>
 
-bool scom(int a, int b)
-{
-    if (a > b)
-        return false;
-    return true;
-}
 int main() {
+    // std::priority_queue<int,std::vector<int>, std::greater<int> > iq;
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
     int num;
     std::cin >> num;
     int temp;
-    std::vector<int> n_list;
-    for(int i=0; i<num; ++i)  {
-        std::cin >>temp;
-        n_list.push_back(temp);
+    std::vector<int> ivec(10001,0);
+    // std::cout << ivec[1] << std::endl;
+    int max=0;
+    for(int i =0; i<num; ++i ) {
+        std::cin >> temp;
+        if (max < temp)
+            max = temp;
+        ivec[temp] += 1;
     }
-    sort(n_list.begin(), n_list.end(), scom);
-
-    for(int i =0; i<num; ++i){
-        std::cout << n_list[i] << "\n";
+    for(int j=0; j<max+1; ++j) {
+        if(ivec[j] == 0)
+            continue;
+        for(int k = 0; k< ivec[j]; ++k) {
+            std::cout << j << "\n";
+        }
     }
 }
+
+
 
 // 메모리.. temp에 저장해서 넣으면 안 되나? 
