b87cff03602c25a834e70dcf84d975249c0ef124
maindishs <manynolan1@gmail.com>
2023-02-12 14:50:19 +0900

    완전탐색

--- cpp/class2/1436.cpp
+++ cpp/class2/1436.cpp
@@ -1,6 +1,19 @@
 // 2 -> 1666
 #include <iostream>
-unsol
-int main() {
+#include <string>
 
+int main() {
+    int inum;
+    std::cin >> inum;
+    std::string ss = "666";
+    int i = 1;
+    int cnt = 0;
+    std::string temp;
+    while(cnt != inum) {
+        ++i;
+        temp = std::to_string(i);
+        if(temp.find(ss) != std::string::npos)
+            ++cnt;    
+    }
+    std::cout << i << std::endl;
 }
