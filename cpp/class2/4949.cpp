e1e50af004f9f91db1a3c0b0dacabd46c95b768c
maindishs <manynolan1@gmail.com>
2023-02-13 00:38:58 +0900

    stack () problem

--- cpp/class2/4949.cpp
+++ cpp/class2/4949.cpp
@@ -2,15 +2,41 @@
 #include <stack>
 
 int main() {
-    std::stack<char> is;
+    // std::stack<char> is;
     std::string temp="";
     while(1) {
         getline(std::cin, temp);
-        if(temp.length() == 1 && temp[0] == '.')
+        if(temp[0] == '.')
             break;
-        std::cout << temp << std::endl; 
+        std::stack<char> is;
+        bool res=true;
+        // std::cout << temp << std::endl; 
         for(int i=0; i< temp.length(); ++i) {
-
-        }   
-    }
+            if(temp[i] == '[' || temp[i] == '(')
+                is.push(temp[i]);
+            else if(temp[i] == ')')
+            {
+                if(is.empty() || is.top() != '(')
+                {   
+                    res = false;
+                    break;
+                }
+                is.pop();
+            }
+            else if (temp[i] == ']') {
+                if(is.empty()||is.top()!='[') {
+                    res = false;
+                    break;
+                }
+                is.pop();
+            }
+        }
+        if (!is.empty())
+            res = false;
+        if(res)
+            std::cout << "yes" << std::endl;
+        else
+            std::cout << "no" << std::endl;
+        // std::cout << is.size() << std::endl;
+    }   
 }
