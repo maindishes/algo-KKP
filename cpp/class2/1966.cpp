67c7ad251b00374f44173fdb157dddc1fcca7552
maindishes <65796820+maindishes@users.noreply.github.com>
2023-02-13 12:45:20 +0900

    solving... : 그 다음 놓은게 없을때 어떻게 해야할지 모르겠음

--- cpp/class2/1966.cpp
+++ cpp/class2/1966.cpp
@@ -1,19 +1,21 @@
 #include <queue>
 #include <vector>
 #include <iostream>
+#include <set>
 // #include <pair>
 
-int sol(std::deque<std::pair<int,int> > &iq) {
-    while(!iq.empty()) {
+// int sol(std::deque<std::pair<int,int> > &iq) {
+//     while(!iq.empty()) {
         
 
-    }
-}
+//     }
+// }
 
 int main() {
     int inum;
     std::cin >> inum;
     std::deque<std::pair<int, int> > iq;
+    std::set<int, std::greater<int> > iset;
     int a,b;
     int temp;
     for(int i=0; i<inum; ++i) {
@@ -22,6 +24,18 @@ int main() {
         for(int j =0; j< a; ++j) {
             std::cin >> temp;
             iq.push_back(std::make_pair(temp, j));
+            iset.insert(temp);
+        }
+        // std::deque<std::
+        // for (auto k : iset) {
+        //     std::cout << k << std::endl;
+        // }
+        sort(iq.begin(),iq.end(),std::greater<std::pair<int,int> >());
+        for(int k=0;k<iq.size()-1; ++k) {
+            std::cout << iq[k].first << " : " << iq[k].second << std::endl;
+            if(iq[k].second == b) {
+                std::cout << k << std::endl;
+            }
         }
     }
 }
