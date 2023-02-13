47e5ee254f981b0c5404d47c9aa94a665e725ac0
maindishes <65796820+maindishes@users.noreply.github.com>
2023-02-13 11:26:46 +0900

    이상한 예외 고민하다

--- cpp/class2/11866.cpp
+++ cpp/class2/11866.cpp
@@ -1,3 +1,10 @@
+/**
+ * 요세푸스0 문제 
+ * 2023 - 02 - 13
+ * 입력:
+ 7 3
+*/
+
 #include <iostream>
 #include <vector>
 #include <deque>
@@ -9,10 +16,44 @@ int main() {
     int f =0;
     int r = 0;
     for(int i=0; i<n; ++i) {
-        my_vec.push_back(i+1);
+        iq.push_back(1+i);
     }
-    int r = my_vec.size();
-    for (int i =0; i<)
-
+    int a,b;
+    std::cout << "<";
+    while(1) {
+        int temp;
+        // if(iq.size() == k-1) {
+        //     for(int i =0; i<k-2; ++i) {
+        //         if (iq.size() == 2) {
+        //             for(int k =0; k<2; ++k)
+        //                 std::cout << iq[k%2] << ">";
+        //             break;
+        //         }
+        //         std::cout << iq[(i)%(k-1)] << ", ";
+        //         temp = iq.front();
+        //         iq.pop_front();
+        //         iq.push_back(temp);
+        //     }
+        //     std::cout << ">";
+        //     break;
+        // }
+        if(iq.size() == 1)
+        {
+            std::cout << iq[0];
+            std::cout << ">";
+            break;
+        }
+        for(int i =0; i<k-1; ++i) {
+            // if(iq.size() == 3)
+            //     break;
+            a = iq.front();
+            iq.push_back(a);
+            iq.pop_front();
+        }
+        b = iq.front();
+        iq.pop_front();
+        std::cout << b << ", ";
+    }
+    // std::cout << ">" << std::endl;
 }
 
