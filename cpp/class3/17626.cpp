281c2b7a52f30cf2ca7d78344f83cd3e25319772
maindishes <manynolan1@gmail.com>
2023-03-15 15:39:56 +0900

    2023 3 15

--- cpp/class3/17626.cpp
+++ cpp/class3/17626.cpp
@@ -3,3 +3,28 @@
  * 2023 3 12
 25
 */
+
+
+#include <iostream>
+#include <array>
+#include <algorithm>
+
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    int n; 
+    std::cin >> n;  
+    std::array<int, 50001> cache;
+    cache.fill(99999);
+    cache[0] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j * j <= i; j++)
+        {
+            cache[i] = std::min(cache[i], cache[i - j * j] + 1);
+        }
+    }
+    
+    std::cout << cache[n];
+    return 0;
+}
